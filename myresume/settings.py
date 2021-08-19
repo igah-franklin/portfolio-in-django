df985b3d9e9c6f21d0f7a8452c6f8b33b0874bc3
igah-franklin <igahfranklyn@gmail.com>
2021-08-19 15:27:44 +0100

    default settings

--- myresume/settings.py
+++ myresume/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-n+_5f04&9vycnkooq(_xyhaveo(la2(_a$_)ug1jvy!wmgrtm$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = ['igahfranklin.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
