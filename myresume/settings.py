4f892c7249ab4848d980185cfb2677b0a7a09e3c
igah-franklin <igahfranklyn@gmail.com>
2021-08-19 19:50:27 +0100

    static files

--- myresume/settings.py
+++ myresume/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from typing import cast
 from decouple import config
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -155,3 +156,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+django_heroku.settings(locals())
