384307f901dabe5d1e5fb1a5068bdeb6af21f72e
igah-franklin <igahfranklyn@gmail.com>
2021-08-19 20:09:22 +0100

    storage

--- myresume/settings.py
+++ myresume/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = '/static/'
 
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
