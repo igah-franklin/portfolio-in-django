2532d02d4ed128a37f17575e419da4ecdde3d9be
igah-franklin <igahfranklyn@gmail.com>
2021-08-19 23:04:08 +0100

    static files

--- myresume/settings.py
+++ myresume/settings.py
@@ -159,4 +159,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
